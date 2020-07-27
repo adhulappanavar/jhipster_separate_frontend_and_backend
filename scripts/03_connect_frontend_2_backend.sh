299ed74a45a5d44719249d4c7b5f7b7455ce5a8f
Anil Dhulappanavar <dhulappanavar@yahoo.com>
2020-07-27 11:03:02 +0530

    replacements

--- scripts/03_connect_frontend_2_backend.sh
+++ scripts/03_connect_frontend_2_backend.sh
@@ -3,11 +3,12 @@ cd frontend-webapp/CustomerUI
 
 sed -i '' "s|isAuthenticated = action.payload && action.payload.data && action.payload.data.activated|isAuthenticated = true|g" src/main/webapp/app/shared/reducers/authentication.ts
 
-sed -i '' "s|target:|target: \`http://40.81.12.242:8761\`, // |g"  webpack/webpack.dev.js 
+sed -i '' "1,/target/s/target/target2/"  webpack/webpack.dev.js 
+sed -i '' "s|target2:|target: \`http://13.87.224.189:8761\`, // |g"  webpack/webpack.dev.js 
 
 sed -i '' "s|'api/addresses'|'services/customerservice/api/addresses'|g" src/main/webapp/app/entities/address/address.reducer.ts
 
-sed -i '' "s|'api/customers'|'services/customerservice/api/addresses'|g" src/main/webapp/app/entities/customer/customer.reducer.ts
+sed -i '' "s|'api/customers'|'services/customerservice/api/customers'|g" src/main/webapp/app/entities/customer/customer.reducer.ts
 
 sed -i '' "s|'api/account'|'services/customerservice/api/account'|g" src/main/webapp/app/modules/account/password/password.reducer.ts
 
